fa2c5a75514ed37203c2d2fe78c1962f9d8070a3
Chris Stehlar (RIT Student) <cms8904@rit.edu>
2024-08-29 10:42:05 -0400

    started greeter

---
+++ demos/greeter-script.js
@@ -0,0 +1,10 @@
+
+// 1 - get a reference to the button
+// 2 - add a click event to button that calls a `sayHello` function
+// 3 - create a `sayHello()` function
+// 3A - get name of person from the <input>
+// 3B - get a reference to the #output <p>
+// 3C - update HTML of #output <p>
+console.log(a);
+
+var a = 1;
