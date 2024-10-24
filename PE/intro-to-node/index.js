05d6609b7da58417146b60e23614e97d301a9062
Chris Stehlar (RIT Student) <cms8904@rit.edu>
2024-10-24 10:20:57 -0400

    node stuff

---
+++ PE/intro-to-node/index.js
@@ -0,0 +1,13 @@
+console.log("Hello World!");
+
+const request = require('request');
+
+let options = {
+    url: 'https://geek-jokes.sameerkumar.website/api?format=json',
+    method: 'GET'
+}
+
+request(options, (err, response, body) => {
+    if(!err && response.statusCode == 200)
+        console.log(body)
+});
