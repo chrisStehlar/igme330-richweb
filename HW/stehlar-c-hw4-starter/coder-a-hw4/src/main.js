dd0d2cf4a4af317492e5dac3ca1e645bd02fce63
Chris Stehlar (RIT Student) <cms8904@rit.edu>
2024-12-02 15:54:15 -0500

    not sure

--- HW/stehlar-c-hw4-starter/coder-a-hw4/src/main.js
+++ HW/stehlar-c-hw4-starter/coder-a-hw4/src/main.js
@@ -3,8 +3,8 @@ import * as ajax from "./ajax.js";
 
 // I. Variables & constants
 // NB - it's easy to get [longitude,latitude] coordinates with this tool: http://geojson.io/
-const lnglatNYS = [-75.71615970715911, 43.025810763917775];
-const lnglatUSA = [-98.5696, 39.8282];
+const longlatNYS = [-75.71615970715911, 43.025810763917775];
+const longlatUSA = [-98.5696, 39.8282];
 
 
 // II. Functions
@@ -18,7 +18,7 @@ const setupUI = () => {
 }
 
 const init = () => {
-	map.initMap(lnglatNYS);
+	map.initMap(longlatNYS);
 	setupUI();
 };
 
