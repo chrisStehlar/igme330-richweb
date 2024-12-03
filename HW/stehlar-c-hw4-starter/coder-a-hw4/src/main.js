4be827efa1c8074702111068244961706a3da2f7
chris <cms8904@g.rit.edu>
2024-12-02 23:22:25 -0500

    finished hw4 starter

--- HW/stehlar-c-hw4-starter/coder-a-hw4/src/main.js
+++ HW/stehlar-c-hw4-starter/coder-a-hw4/src/main.js
@@ -6,6 +6,7 @@ import * as ajax from "./ajax.js";
 const longlatNYS = [-75.71615970715911, 43.025810763917775];
 const longlatUSA = [-98.5696, 39.8282];
 let geojson;
+let favoriteIds = ["p20", "p79", "p180", "p43"];
 
 
 // II. Functions
@@ -31,6 +32,34 @@ const setupUI = () => {
 		map.flyTo(longlatUSA);
 	};
 
+	refreshFavorites();
+}
+
+const refreshFavorites = () => {
+	const favoritesContainer = document.querySelector("#favorites-list");
+	favoritesContainer.innerHTML = "";
+	for(const id of favoriteIds) {
+		favoritesContainer.appendChild(createFavoriteElement(id));
+	}
+}
+
+const createFavoriteElement = (id) => {
+	const feature = getFeatureById(id);
+	const a = document.createElement("a");
+	a.className = "panel-block";
+	a.id = feature.id;
+	a.onclick = () => {
+		showFeatureDetails(a.id);
+		map.setZoomLevel(6);
+		map.flyTo(feature.geometry.coordinates);
+	};
+	a.innerHTML = `
+		<span class="panel-icon">
+			<i class="fas fa-map-pin"></i>
+		</span>
+		${feature.properties.title}
+	`;
+	return a;
 }
 
 const init = () => {
@@ -46,6 +75,27 @@ const init = () => {
 
 const showFeatureDetails = (id) => {
 	console.log(`showFeatureDetails(${id})`);
+
+	const feature = getFeatureById(id);
+	document.querySelector("#details-1").innerHTML = `Info for ${feature.properties.title}`;
+
+	document.querySelector("#details-2").innerHTML = 
+		`<div>
+			<div><b>Address:</b> ${feature.properties.address}</div>
+			<div><b>Phone:</b> ${feature.properties.phone}</div>
+			<div><b>Website:</b> ${feature.properties.url}</div>
+		</div>`;
+
+	document.querySelector("#details-3").innerHTML = `${feature.properties.description}`;
 };
+
+const getFeatureById = (id) => {
+	for (const feature of geojson.features) {
+		if (feature.id === id) {
+			return feature;
+		}
+	}
+	return null;
+}
 
 init();
